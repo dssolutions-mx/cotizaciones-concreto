62a630759eed709ec9e7647ed03b95c46ab04d1d
Juan Aguirre <juan.aguirre@dssolutions-mx.com>
2025-10-06 09:55:06 -0600

    'Added loading state to orders page'

--- src/app/client-portal/orders/[id]/page.tsx
+++ src/app/client-portal/orders/[id]/page.tsx
@@ -200,8 +200,10 @@ export default function OrderDetailPage() {
             </div>
           </div>
         </motion.div>
+        </Container>
 
         {/* Deliveries Timeline */}
+        <Container maxWidth="2xl" className="py-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
